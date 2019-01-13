fcad2291760791f32e63a2317eb522bee877f53a
ZeroRaws <xenus32@gmail.com>
2019-01-13 20:41:38 +0500

    Добавление кнопки Set Tags в админку, для создания связи между статьей и тегами

--- modules/admin/controllers/ArticleController.php
+++ modules/admin/controllers/ArticleController.php
@@ -4,6 +4,7 @@ namespace app\modules\admin\controllers;
 
 use app\models\Category;
 use app\models\ImageUpload;
+use app\models\Tag;
 use Yii;
 use app\models\Article;
 use app\models\ArticleSearch;
@@ -157,6 +158,21 @@ class ArticleController extends Controller
             'article'=>$article,
             'selectedCategory'=>$selectedCategory,
             'categories'=>$categories,
+        ]);
+    }
+
+    public function actionSetTags($id){
+        $article = $this->findModel($id);
+        $selectedTags = $article->getSelectedTags();
+        $tags = ArrayHelper::map(Tag::find()->all(), 'id', 'title');
+        if (yii::$app->request->isPost){
+            $tags = yii::$app->request->post('tags');
+            $article->saveTags($tags);
+            return $this->redirect(['view', 'id'=>$article->id]);
+        }
+        return $this->render('tags', [
+            'selectedTags'=>$selectedTags,
+            'tags'=>$tags,
         ]);
     }
 }
--- modules/admin/views/article/tags.php
+++ modules/admin/views/article/tags.php
@@ -12,7 +12,7 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= html::dropDownList('category', $selectedCategory, $categories, ['class'=>'form-control']) ?>
+    <?= html::dropDownList('tags', $selectedTags, $tags, ['class'=>'form-control', 'multiple'=>true]) ?>
 
     <div class="form-group">
 
